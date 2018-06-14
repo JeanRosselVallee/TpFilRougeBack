0b1f9546d600f10200710fe152dbc568335c67b0
John1720 <35218552+John1720@users.noreply.github.com>
2018-06-14 23:31:55 +0200

    Add comments to new methods

--- src/shop/CartController.java
+++ src/shop/CartController.java
@@ -8,23 +8,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import shop.model.CartModel;
 import shop.model.CartViewModel;
 
-//@WebServlet( name="Cart", urlPatterns = {"/Cart", "/Cart/add", "/Cart/rem"} )
-
+/**
+ * Offers the services related to the Cart: 
+ * - get the full cart 
+ * - post a new product to be added to the cart 
+ * - delete an item present in the cart
+ * The url must specify the required service as configured in web.xml 
+ */
 public class CartController extends HttpServlet {
-	
-
-
 	private static final int Long = 0;
-
+	
 	public CartController() {
 		super();
 	}
 
-	
+	/**
+	 * Initialises the cart with the records in the table cart
+	 * Author	Nicolas
+	 * @param  	p_request 
+	 * @param  	p_response 
+	 */
 	public void doGet(
 			HttpServletRequest p_request, 
 			HttpServletResponse p_response
@@ -36,7 +42,7 @@ public class CartController extends HttpServlet {
 		p_response.setHeader("Access-Control-Allow-Origin", "*");
 		p_response.setCharacterEncoding("utf-8");
 
-		// si la connexion à la bdd a échoué alors..
+		// si la connexion Ã  la bdd a Ã©chouÃ© alors..
 		if (CartModel.getInstance().isConnected() == false) 
 		{
 			// on redirige vers la page FailureSQL
@@ -44,48 +50,37 @@ public class CartController extends HttpServlet {
 		} 
 		else // sinon ...
 		{
-			// on récupère le Cartue et on l'affiche
-			//p_request.setAttribute("Cart", CartModel.getInstance().getCart());
-			
-			// Modified by Jean on Tuesday 12 June
-			// Display view of products in cart instead of 2 id's only
 			p_request.setAttribute("CartView", CartViewModel.getInstance().getCart());
-			
-			//this.getServletContext().getRequestDispatcher("/Cart.jsp").forward(p_request, p_response);
 			this.getServletContext().getRequestDispatcher("/CartView.jsp").forward(p_request, p_response);
 		}
 
 	}
-
-	public void doPost(
-
-			HttpServletRequest p_request, HttpServletResponse p_response
-
-	) throws ServletException, IOException {
+	
+	/**
+	 * Adds an item to the cart
+	 * Author	Lila
+	 * @param  	p_request 
+	 * @param  	p_response 
+	 */
+	public void doPost( HttpServletRequest p_request, HttpServletResponse p_response
+		) throws ServletException, IOException {
 		
 		long cartId= 0;  
-		//System.out.println(" Cart Controller doPost ");
 		
 		p_response.setHeader("Access-Control-Allow-Origin", "*");
 		p_response.setCharacterEncoding("utf-8");
 
 		int len = CartModel.getInstance().getCart().size();
 		boolean success = false;
-
 		String s_productid = p_request.getParameter("Product_id");
-		
-		
 		if (s_productid != null) {
 			int Product_id =Integer.parseInt(s_productid);
-
 			CartModel.getInstance().add(cartId, Product_id);
-
 			success = (CartModel.getInstance().getCart().size() > len);
 		}
 
 		if (success == true) {
 			System.out.println("insert success"  );
-
 			this.getServletContext().getRequestDispatcher("/Success.jsp").forward(p_request, p_response);
 		} else {
 			System.out.println("insert failure"  );
@@ -96,55 +91,46 @@ public class CartController extends HttpServlet {
 
 	public void doOptions(HttpServletRequest p_request, HttpServletResponse p_response)
 			throws ServletException, IOException {
-
-
-		//System.out.println(" Cart Controller doOptions ");
-		
 		p_response.setHeader("Access-Control-Allow-Origin", "*");
 		p_response.setHeader("Access-Control-Allow-Headers", "Content-Type");
 		p_response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
 		p_response.setCharacterEncoding("utf-8");
-
 		this.getServletContext().getRequestDispatcher("/Success.jsp").forward(p_request, p_response);
-
 	}
 
-	public void doDelete(
+	/**
+	 * Deletes an item present in the cart
+	 * Author	J. Vallee
+	 * @param  	p_request 
+	 * @param  	p_response 
+	 */
+	public void doDelete( HttpServletRequest p_request, HttpServletResponse p_response
+		) throws ServletException, IOException {
 
-			HttpServletRequest p_request, HttpServletResponse p_response
-
-	) throws ServletException, IOException {
-		
-
-		//System.out.println(" Cart Controller doDelete ");
-		
 		p_response.setHeader("Access-Control-Allow-Origin", "*");
 		p_response.setCharacterEncoding("utf-8");
 
+		/**
+		* Checks the parameters in the request before executing the Delete operation
+		*/
 		boolean success = false;
-		//System.out.println("id=" + p_request.getParameter("id"));
-		//System.out.println("api=" + p_request.getParameter("api") );
 		if (p_request.getParameter("id") != null && p_request.getParameter("api") != null) {
-			//System.out.println(" not null params ");
 			long id = Integer.parseInt(p_request.getParameter("id"));
 			String apiKey = p_request.getParameter("api");
-
 			if (apiKey.equals("azerty123")) {
-				//System.out.println(" apiKey OK");
-
 				success = CartModel.getInstance().removeProductById(id);
-
 			}
 		}
 
+		/**
+		* Notifies the result of the Delete operation
+		*/
 		if (success == true) {
-			System.out.println(" delete succeeded");
+			System.out.println(" deletion of Cart item succeeded");
 			this.getServletContext().getRequestDispatcher("/Success.jsp").forward(p_request, p_response);
 		} else {
-			System.out.println(" delete failed");
+			System.out.println(" deletion of Cart item failed");
 			this.getServletContext().getRequestDispatcher("/Failure.jsp").forward(p_request, p_response);
 		}
-
 	}
-
 }
